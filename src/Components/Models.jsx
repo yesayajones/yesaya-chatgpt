44a963025596f6416b4785ac95fc74a0296d06f3
yesayajones <yesayajones@gmail.com>
2023-04-29 15:06:38 +0200

    feat: Hightlight selected model with green background

--- src/Components/Models.jsx
+++ src/Components/Models.jsx
@@ -1,16 +1,28 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 function Models({ models, currentModel, setCurrentModel, setTemperature }) {
+	const [activeModel, setActiveModel] = useState(null);
+	const handleModelClick = (modelID) => {
+		setActiveModel(modelID);
+	};
 	// Function to generate the top ten models
 	const generateTopTenModels = () => {
 		const topTenModels = models.slice(0, 10);
 		return topTenModels.map((model, index) => (
 			<div
 				key={index}
-				className='p-[12px] border border-white rounded-[5px] text-left transition-[0.25s] hover:bg-[#1a371b] hover:text-white'
-				onClick={() => setCurrentModel(model.id)}
+				className={`w-[200px] h-10 pt-2 px-2 my-4 rounded-md text-start cursor-pointer ${
+					activeModel === model.id
+						? 'bg-[#1a371b] text-white'
+						: 'bg-white hover:bg-[#1a371b]'
+				}`}
+				onClick={() => {
+					setCurrentModel(model.id);
+					handleModelClick(model.id);
+				}}
 			>
-				{model.id}
+				<h3 className='text-lg font-semibold'>{model.id}</h3>
+				<p className='text-gray-500'></p>
 			</div>
 		));
 	};
